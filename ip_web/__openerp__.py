017558d3849f9390910bcd386d12663dbf3bf2ce
Max Mumford <mmu@openerp.com>
2014-04-14 16:51:05 +0200

    [IMP] rename module to be specific to IP as some of the changes are not generic

--- ip_web/__openerp__.py
+++ ip_web/__openerp__.py
@@ -1,12 +1,14 @@
 {
-    'name': 'Website Customer Account',
+    'name': 'IP Web',
     'category': 'Website',
-    'summary': 'More user friendly account for customers. Display things like orders, invoices & tracking codes.',
+    'summary': 'Customer portal and product quantity changes.',
     'version': '1.0',
     'description': """
 Website Customer Account
 ========================
 More user friendly account view for customers. Display things like orders, invoices & tracking codes.
+
+Also show quantity widget when adding products to basket
         """,
     'author': 'Max Mumford (OpenERP)',
     'depends': ['crm','sale','stock','website','website_sale'],
